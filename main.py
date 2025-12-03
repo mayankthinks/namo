3f8cb33669fb836727804cc839e194aaa8364df9
Mayank Kumar <108946605+mayankthinks@users.noreply.github.com>
2025-12-03 08:34:47 +0530

    Fix typo in comment string

--- main.py
+++ main.py
@@ -108,7 +108,7 @@ async def main():
             if stop_flag:
                 break
 
-            comment = f"Ytgrfdwdyr14713196 Bjp {i}"
+            comment = f"Ytgrftdwdyr14713196 Bjp {i}"
             tasks.append(asyncio.create_task(sem_task(comment)))
 
             if i % 50 == 0:

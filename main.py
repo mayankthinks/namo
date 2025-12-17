dd4305c699269ae1b71299ea1258eec661d2185c
Mayank Kumar <108946605+mayankthinks@users.noreply.github.com>
2025-12-17 17:08:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -155,7 +155,7 @@ async def main():
             tasks.append(asyncio.create_task(runner(i)))
 
             if i % 50 == 0:
-                await asyncio.sleep(2)
+                await asyncio.sleep(5)
 
         await asyncio.gather(*tasks, return_exceptions=True)
 

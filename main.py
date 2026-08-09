a5bc7ac1efed2a88e9d32ce6b23b5e747b3423af
Mayank Kumar <108946605+mayankthinks@users.noreply.github.com>
2026-08-10 01:50:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ import random
 def get_config(key, default):
     return os.getenv(key, default)
 
-START_POST_ID = int(get_config("START_POST_ID", "1"))
+START_POST_ID = int(get_config("START_POST_ID", "100"))
 END_POST_ID = int(get_config("END_POST_ID", "100000010000"))
 CONCURRENT_WORKERS = int(get_config("CONCURRENT_WORKERS", "10"))
 DELAY_PER_REQUEST = float(get_config("DELAY_PER_REQUEST", "0.1"))
 PAUSE_INTERVAL = int(get_config("PAUSE_INTERVAL", "50"))
-PAUSE_DURATION = float(get_config("PAUSE_DURATION", "60"))
+PAUSE_DURATION = float(get_config("PAUSE_DURATION", "1"))
 REQUEST_TIMEOUT = int(get_config("REQUEST_TIMEOUT", "30"))
 MAX_RETRIES = int(get_config("MAX_RETRIES", "3"))
 
